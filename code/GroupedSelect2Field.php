98981be2ef8103fe3db31ba4e323906f46a0c778
Shadae Ellmers <shadae.ellmers@signify.co.nz>
2025-03-18 10:38:28 +1300

    fix: remove outdated jquery / entwine requirements. moj144-7

--- code/GroupedSelect2Field.php
+++ code/GroupedSelect2Field.php
@@ -9,8 +9,6 @@ class GroupedSelect2Field extends GroupedDropdownField
 {
     public function Field($properties = array())
     {
-        Requirements::javascript('silverstripe/admin: thirdparty/jquery/jquery.js');
-        Requirements::javascript('silverstripe/admin: thirdparty/jquery-entwine/dist/jquery.entwine-dist.js');
         Requirements::javascript('sheadawson/silverstripe-select2: select2/select2.js');
         Requirements::javascript('sheadawson/silverstripe-select2: javascript/ajaxselect2.init.js');
         Requirements::css('sheadawson/silverstripe-select2: select2/select2.min.css');
