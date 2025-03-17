f78281396a2d2faee819cf2c99f691b3ff0e0d7e
Shadae Ellmers <shadae.ellmers@signify.co.nz>
2025-03-18 10:34:36 +1300

    feat: replace deprecated methods. moj144-7

--- code/AjaxSelect2Field.php
+++ code/AjaxSelect2Field.php
@@ -72,7 +72,7 @@ class AjaxSelect2Field extends TextField
         );
 
         $originalSourceFileComments = Config::inst()->get('SSViewer', 'source_file_comments');
-        Config::inst()->update('SSViewer', 'source_file_comments', false);
+        Config::modify()->set('SSViewer', 'source_file_comments', false);
         foreach ($results as $object) {
             $return['list'][] = array(
                 'id' => $object->ID,
@@ -80,9 +80,11 @@ class AjaxSelect2Field extends TextField
                 'selectionContent' => SSViewer::fromString($this->getConfig('selectionFormat'))->process($object)
             );
         }
-        Config::inst()->update('SSViewer', 'source_file_comments', $originalSourceFileComments);
+        Config::modify()->set('SSViewer', 'source_file_comments', $originalSourceFileComments);
 
-        return Convert::array2json($return);
+        $jsonData = json_encode($return);
+
+        return $jsonData;
     }
 
     public function setConfig($k, $v)
@@ -114,11 +116,11 @@ class AjaxSelect2Field extends TextField
             )
         );
 
-        if ($this->Value() && $object = DataObject::get($this->getConfig('classToSearch'))->byID($this->Value())) {
+        if ($this->Value() && $object = $this->getConfig('classToSearch')::get()->byID($this->Value())) {
             $originalSourceFileComments = Config::inst()->get('SSViewer', 'source_file_comments');
-            Config::inst()->update('SSViewer', 'source_file_comments', false);
+            Config::modify()->set('SSViewer', 'source_file_comments', false);
             $attributes['data-selectioncontent'] = html_entity_decode(SSViewer::fromString($this->getConfig('selectionFormat'))->process($object));
-            Config::inst()->update('SSViewer', 'source_file_comments', $originalSourceFileComments);
+            Config::modify()->set('SSViewer', 'source_file_comments', $originalSourceFileComments);
         }
 
         return $attributes;
